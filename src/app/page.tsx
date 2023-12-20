a73b9b9b50a0daa606d532c4fd4381d75a63aaf7
estaniulyte <43463977+estaniulyte@users.noreply.github.com>
2023-12-20 10:50:40 +0200

    change: change cover pic

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,9 +6,9 @@ export default function Home() {
       <div className='h-screen'>
         <Image
           className=' sm:m-auto w-full object-cover '
-          src='/profile3.jpg'
+          src='/profile.jpg'
           fill={true}
-          alt='Picture of the author'
+          alt='Photoshot pictures'
         />
       </div>
       <div className='h-[100px] flex justify-center mt-10 '>COMMING SOON</div>
