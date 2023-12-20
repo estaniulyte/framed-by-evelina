9aa8ec777d70eb3e22da21baf6bb599e40caaa5a
estaniulyte <43463977+estaniulyte@users.noreply.github.com>
2023-12-20 11:56:47 +0200

    feat: install swiper and add dynamic carousel

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,59 @@
+'use client';
+
 import Image from 'next/image';
+
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+import { Autoplay, EffectFade } from 'swiper/modules';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/effect-fade';
 
 export default function Home() {
   return (
     <main className=''>
-      <div className='h-screen'>
+      {/* <div className='h-screen'>
         <Image
           className=' sm:m-auto w-full object-cover '
           src='/profile.jpg'
           fill={true}
           alt='Photoshot pictures'
         />
-      </div>
+      </div> */}
+      <Swiper
+        spaceBetween={0}
+        effect={'fade'}
+        loop={true}
+        centeredSlides={true}
+        autoplay={{
+          delay: 2500,
+          disableOnInteraction: false,
+        }}
+        modules={[EffectFade, Autoplay]}
+        className='mySwiper'
+      >
+        <SwiperSlide>
+          <div className='h-screen'>
+            <Image
+              className=' sm:m-auto w-full object-cover '
+              src='/profile.jpg'
+              fill={true}
+              alt='Photoshot pictures'
+            />
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div className='h-screen'>
+            <Image
+              className=' sm:m-auto w-full object-cover '
+              src='/profile3.jpg'
+              fill={true}
+              alt='Photoshot pictures'
+            />
+          </div>
+        </SwiperSlide>
+      </Swiper>
       <div className='h-[100px] flex justify-center mt-10 '>COMMING SOON</div>
     </main>
   );
