87f07d0ded7cff8776fcae82cf72d2b725a86585
estaniulyte <43463977+estaniulyte@users.noreply.github.com>
2023-12-20 15:37:38 +0200

    different images for mobile

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,7 +12,8 @@ import 'swiper/css/effect-fade';
 
 export default function Home() {
   return (
-    <main className=''>
+    // <main className='hidden lg:block'>
+    <main>
       {/* <div className='h-screen'>
         <Image
           className=' sm:m-auto w-full object-cover '
@@ -21,49 +22,96 @@ export default function Home() {
           alt='Photoshot pictures'
         />
       </div> */}
-      <Swiper
-        spaceBetween={0}
-        effect={'fade'}
-        loop={true}
-        centeredSlides={true}
-        autoplay={{
-          delay: 2500,
-          disableOnInteraction: false,
-        }}
-        modules={[EffectFade, Autoplay]}
-        className='mySwiper'
-      >
-        <SwiperSlide>
-          <div className='h-screen'>
-            <Image
-              className=' sm:m-auto w-full object-cover '
-              src='/profile.jpg'
-              fill={true}
-              alt='Photoshot pictures'
-            />
-          </div>
-        </SwiperSlide>
-        <SwiperSlide>
-          <div className='h-screen'>
-            <Image
-              className=' sm:m-auto w-full object-cover '
-              src='/profile3.jpg'
-              fill={true}
-              alt='Photoshot pictures'
-            />
-          </div>
-        </SwiperSlide>
-        <SwiperSlide>
-          <div className='h-screen'>
-            <Image
-              className=' sm:m-auto w-full object-cover '
-              src='/profile4.jpg'
-              fill={true}
-              alt='Photoshot pictures'
-            />
-          </div>
-        </SwiperSlide>
-      </Swiper>
+      <div className='block lg:hidden'>
+        <Swiper
+          spaceBetween={0}
+          effect={'fade'}
+          loop={true}
+          centeredSlides={true}
+          autoplay={{
+            delay: 2500,
+            disableOnInteraction: false,
+          }}
+          modules={[EffectFade, Autoplay]}
+          className='mySwiper'
+        >
+          <SwiperSlide>
+            <div className='h-screen'>
+              <Image
+                className=' sm:m-auto w-full object-cover '
+                src='/book-mobile.jpg'
+                fill={true}
+                alt='Photoshot pictures'
+              />
+            </div>
+          </SwiperSlide>
+          <SwiperSlide>
+            <div className='h-screen'>
+              <Image
+                className=' sm:m-auto w-full object-cover '
+                src='/couple-mobile.jpg'
+                fill={true}
+                alt='Photoshot pictures'
+              />
+            </div>
+          </SwiperSlide>
+          <SwiperSlide>
+            <div className='h-screen'>
+              <Image
+                className=' sm:m-auto w-full object-cover '
+                src='/mocas-mobile.jpg'
+                fill={true}
+                alt='Photoshot pictures'
+              />
+            </div>
+          </SwiperSlide>
+        </Swiper>
+      </div>
+      <div className='hidden lg:block'>
+        <Swiper
+          spaceBetween={0}
+          effect={'fade'}
+          loop={true}
+          centeredSlides={true}
+          autoplay={{
+            delay: 2500,
+            disableOnInteraction: false,
+          }}
+          modules={[EffectFade, Autoplay]}
+          className='mySwiper'
+        >
+          <SwiperSlide>
+            <div className='h-screen'>
+              <Image
+                className=' sm:m-auto w-full object-cover '
+                src='/profile.jpg'
+                fill={true}
+                alt='Photoshot pictures'
+              />
+            </div>
+          </SwiperSlide>
+          <SwiperSlide>
+            <div className='h-screen'>
+              <Image
+                className=' sm:m-auto w-full object-cover '
+                src='/profile3.jpg'
+                fill={true}
+                alt='Photoshot pictures'
+              />
+            </div>
+          </SwiperSlide>
+          <SwiperSlide>
+            <div className='h-screen'>
+              <Image
+                className=' sm:m-auto w-full object-cover '
+                src='/profile4.jpg'
+                fill={true}
+                alt='Photoshot pictures'
+              />
+            </div>
+          </SwiperSlide>
+        </Swiper>
+      </div>
       <div className='h-[100px] flex justify-center mt-10 '>COMMING SOON</div>
     </main>
   );
