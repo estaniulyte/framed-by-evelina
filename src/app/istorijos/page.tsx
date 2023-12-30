0b5cbaf0011a44e19b0d215874ac726892222a84
estaniulyte <43463977+estaniulyte@users.noreply.github.com>
2023-12-30 21:38:02 +0200

    add tabs

--- src/app/istorijos/page.tsx
+++ src/app/istorijos/page.tsx
@@ -34,19 +34,83 @@ export default function Gallery() {
             </h1>
           </div>
         </div>
-        <p className='pb-5 text-center uppercase'>COMING SOON</p>
-        {/* <Tabs defaultValue='account' className='w-full'>
-          <TabsList>
+        <Tabs defaultValue='all' className='w-full mx-auto md:my-5 pb-5'>
+          <TabsList className='flex mb-7'>
+            <TabsTrigger value='all' className=''>
+              Visos
+            </TabsTrigger>
             <TabsTrigger value='couples'>Poros</TabsTrigger>
             <TabsTrigger value='personal'>Asmeninės</TabsTrigger>
             <TabsTrigger value='family'>Šeimos</TabsTrigger>
             <TabsTrigger value='products'>Produktai</TabsTrigger>
           </TabsList>
-          <TabsContent value='couples'>
-            Make changes to your account here.
+          <TabsContent value='all'>
+            <div className='grid grid-cols-1 md:grid-cols-2 gap-y-7 gap-x-7'>
+              <div className='grid grid-cols-1 md:grid-cols-2 gap-x-4 bg-slate-100'>
+                <div>
+                  <img
+                    className='w-full aspect-[5/6] object-cover'
+                    src='https://drive.google.com/uc?export=download&id=1dyTME0m3jl7O59gL_jMER0-9a1fw9k53'
+                  ></img>
+                </div>
+                <div className='w-[80%] py-8 md:py-0 text-center m-auto text-xl 3xl:text-3xl tracking-wider leading-7'>
+                  Žieminis pasivaiksčiojimas ir meškučių medžioklė
+                </div>
+              </div>
+              <div className='grid grid-cols-1 md:grid-cols-2 gap-x-4 bg-slate-100'>
+                <div>
+                  <img
+                    className='w-full aspect-[5/6] object-cover'
+                    src='https://drive.google.com/uc?export=download&id=1YZZ-luI9q_NQey0H_f2NgJOTFFRM5eyL'
+                  ></img>
+                </div>
+                <div className='w-[80%] py-8 md:py-0 text-center m-auto text-xl 3xl:text-3xl tracking-wider leading-7'>
+                  Young ir Kauno eglutė
+                </div>
+              </div>
+              <div className='grid grid-cols-1 md:grid-cols-2 gap-x-4 bg-slate-100'>
+                <div>
+                  <img
+                    className='w-full aspect-[5/6] object-cover'
+                    src='https://drive.google.com/uc?export=download&id=18ScN1aVFDfH4UwTaSYNGqXaoBacqHczv'
+                  ></img>
+                </div>
+                <div className='w-[80%] py-8 md:py-0 text-center m-auto text-xl 3xl:text-3xl tracking-wider leading-7'>
+                  Atsinaujint LinkedIn portretą
+                </div>
+              </div>
+              <div className='grid grid-cols-1 md:grid-cols-2 gap-x-4 bg-slate-100'>
+                <div>
+                  <img
+                    className='w-full aspect-[5/6] object-cover'
+                    src='https://drive.google.com/uc?export=download&id=1uCi7KrsGRFs4Q_OKy9ptT_zamU33n4CI'
+                  ></img>
+                </div>
+                <div className='w-[80%] py-8 md:py-0 text-center m-auto text-xl 3xl:text-3xl tracking-wider leading-7'>
+                  {' '}
+                  Dominika ir jos knygų karalystė
+                </div>
+              </div>
+              <div className='grid grid-cols-1 md:grid-cols-2 gap-x-4 bg-slate-100'>
+                <div>
+                  <img
+                    className='w-full aspect-[5/6] object-cover'
+                    src='https://drive.google.com/uc?export=download&id=1V72m07BxFnO38pj5YJW1UnAsAHadPoMj'
+                  ></img>
+                </div>
+                <div className='w-[80%] py-8 md:py-0 text-center m-auto text-xl 3xl:text-3xl tracking-wider leading-7'>
+                  {' '}
+                  Glorijos 1-asis gimtadieniukas
+                </div>
+              </div>
+            </div>
           </TabsContent>
-          <TabsContent value='password'>Change your password here.</TabsContent>
-        </Tabs> */}
+          <TabsContent value='couples'>Poros</TabsContent>
+          <TabsContent value='personal'>Asmenines</TabsContent>
+          <TabsContent value='family'>Seimos</TabsContent>
+          <TabsContent value='products'>Produktai</TabsContent>
+        </Tabs>
+        <p className='pb-5 text-center uppercase'>MORE COMING SOON</p>
       </MaxWidthWrapper>
     </main>
   );
