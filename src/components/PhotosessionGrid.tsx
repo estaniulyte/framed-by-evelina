062054e910f78342954ad8588932e0856e339303
estaniulyte <43463977+estaniulyte@users.noreply.github.com>
2024-01-02 14:54:50 +0200

    change: add navigation button

--- src/components/PhotosessionGrid.tsx
+++ src/components/PhotosessionGrid.tsx
@@ -1,3 +1,6 @@
+import Link from 'next/link';
+import { Button } from './ui/button';
+
 interface Item {
   name: string;
   slug: string;
@@ -21,13 +24,24 @@ const PhotosessionGrid = ({ items }: Props) => {
             className='grid grid-cols-1 md:grid-cols-2 gap-x-4 bg-slate-100'
           >
             <div>
-              <img
-                className='w-full aspect-[5/6] object-cover'
-                src={item.picture}
-              ></img>
+              <Link href={item.slug}>
+                <img
+                  className='w-full aspect-[5/6] object-cover'
+                  src={item.picture}
+                ></img>
+              </Link>
             </div>
-            <div className='w-[80%] py-8 md:py-0 text-center m-auto text-xl 3xl:text-3xl tracking-wider leading-7'>
-              {item.description}{' '}
+            <div className='w-[80%] py-8 md:py-0 text-center m-auto text-xl 3xl:text-3xl tracking-wider leading-7 flex-column'>
+              <div>{item.description}</div>
+              <Link href={item.slug}>
+                <Button
+                  size='lg'
+                  variant='default'
+                  className='uppercase bg-[#d9ab7b] text-white mt-7 hover:bg-[#d9ab7b]/90 '
+                >
+                  Peržiūrėti
+                </Button>
+              </Link>
             </div>
           </div>
         );
