a264dc51938d86aa0dff968d44a8e48b1cd8e406
estaniulyte <43463977+estaniulyte@users.noreply.github.com>
2024-01-02 19:09:17 +0200

    change: remove unused logic

--- src/app/istorijos/[slug]/page.tsx
+++ src/app/istorijos/[slug]/page.tsx
@@ -29,19 +29,6 @@ const Page = async ({ params }: PageProps) => {
 
   const { name, description, pictures } = selectedPhotoshoot;
 
-  const [index, setIndex] = useState(-1);
-
-  const currentImage = pictures[index];
-  const nextIndex = (index + 1) % pictures.length;
-  const nextImage = pictures[nextIndex] || currentImage;
-  const prevIndex = (index + pictures.length - 1) % pictures.length;
-  const prevImage = pictures[prevIndex] || currentImage;
-
-  const handleClick = (index: number) => setIndex(index);
-  const handleClose = () => setIndex(-1);
-  const handleMovePrev = () => setIndex(prevIndex);
-  const handleMoveNext = () => setIndex(nextIndex);
-
   return (
     <main className=' w-full'>
       <div className='h-[60px] md:h-[140px] mb-6 bg-black w-full'></div>
