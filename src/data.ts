f472bae2e7874f4f0f94a8dc7af22b0da4fa1c2b
estaniulyte <43463977+estaniulyte@users.noreply.github.com>
2024-01-02 19:32:11 +0200

    change: uncomment one pic

--- src/data.ts
+++ src/data.ts
@@ -39,12 +39,12 @@ export const photoshootList = [
         width: 2,
         height: 3,
       },
-      // {
-      //   src: 'https://drive.google.com/uc?export=download&id=1Ba2qCEwO5DC8BbKR1jI0dH5BCofr6WAY',
-      //   alt: '',
-      //   width: 2,
-      //   height: 3,
-      // },
+      {
+        src: 'https://drive.google.com/uc?export=download&id=1Ba2qCEwO5DC8BbKR1jI0dH5BCofr6WAY',
+        alt: '',
+        width: 2,
+        height: 3,
+      },
       // {
       //   src: 'https://drive.google.com/uc?export=download&id=1ubBozN5PCzMdEAOPibdKD6-Ab0s_RxfI',
       //   alt: '',
